f8bd39cb27bf5e0deee9d63f9b9e5d0d9b9a6a18
azertypow <nico@villa1203.ch>
2024-12-16 16:23:13 +0100

    feat: first admin struct

---
+++ site/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The config file is optional. It accepts a return array with config options
+ * Note: Never include more than one return statement, all options go within this single return array
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
+ * This setting must be set to false in production.
+ * All config options: https://getkirby.com/docs/reference/system/options
+ */
+return [
+    'debug' => true,
+    'routes' => [
+        [
+            'pattern' => '/',
+            'action' => function () {
+                go('panel');
+            },
+        ],
+        [
+            'pattern' => '/infomaniak/list-of-event-name',
+            'action' => function() {
+                include_once 'utils/infomaniak_getListOfEvent.php';
+                header("Access-Control-Allow-Origin: *");
+
+                return infomaniak_getListOfEvent();
+            },
+        ],
+    ],
+    'panel' => [
+        'css' => 'site/plugins/custom-panel/css/main.css'
+    ]
+];
---
+++ utils/infomaniak_getListOfEvent.php
@@ -0,0 +1,9 @@
+<?php
+
+function infomaniak_getListOfEvent()
+{
+
+    return \Kirby\Cms\Response::json([
+        'bonjour' => 'hello'
+    ]);
+}
