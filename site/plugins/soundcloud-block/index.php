6eef6992b567962f52fe0626aeca8e5f1022bb79
azertypow <nico@villa1203.ch>
2025-11-26 10:06:47 +0100

    feat: clean unnecessary code

--- site/plugins/soundcloud-block/index.php
+++ site/plugins/soundcloud-block/index.php
@@ -1,11 +1,3 @@
 <?php
 
-Kirby::plugin('grutli/soundcloud-block', [
-    'snippets' => [
-        'blocks/soundCloudPlayer' => __DIR__ . '/snippets/soundCloudPlayer.php'
-    ],
-    'assets' => [
-        'index.js' => __DIR__ . '/index.js',
-        'index.css' => __DIR__ . '/index.css'
-    ]
-]);
+Kirby::plugin('grutli/soundcloud-block', []);
